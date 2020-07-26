fe967a271ad32889b5e50932ad12eabb2a46e474
osamudiamen <osas.azamegbe@gmail.com>
2020-07-27 00:42:41 +0100

    npm and esm installed. Working

--- index.js
+++ index.js
@@ -4,3 +4,7 @@ const dbx = new Dropbox({
     accessToken: 'aVZ_hXoMCm8AAAAAAAACgWv0lXw7f59mTFyuUR1CEMnALuqq6F7zFWmGsaSPOdIZ',
     fetch
 });
+
+dbx.filesListFolder({
+    path: '',
+}).then(res => console.log(res.slice(0,100)));
